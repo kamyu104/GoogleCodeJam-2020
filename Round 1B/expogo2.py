d2b63cfb022ada7f27007622d540b64d64fc2a33
Allen Liu <kamyu104@gmail.com>
2020-04-20 05:37:46 +0800

    update

---
+++ Round 1B/expogo2.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2020 kamyu. All rights reserved.
+#
+# Google Code Jam 2020 Round 1B - Problem A. Expogo
+# https://codingcompetitions.withgoogle.com/codejam/round/000000000019fef2/00000000002d5b62
+#
+# Time:  O(logX + logY)
+# Space: O(1)
+#
+
+def expogo():
+    X, Y = map(int, raw_input().strip().split())
+    if (abs(X)+abs(Y))%2 == 0:
+        return "IMPOSSIBLE"
+    N, total = 1, 0
+    while not (total >= abs(X)+abs(Y)):
+        total += N
+        N *= 2
+    N //= 2
+    result = []
+    while N:
+        if abs(X) > abs(Y):
+            if X > 0: 
+                result.append('E')
+                X -= N
+            else:
+                result.append('W')
+                X += N
+        else:
+            if Y > 0:
+                result.append('N')
+                Y -= N
+            else:
+                result.append('S')
+                Y += N
+        N //= 2
+    return "".join(reversed(result))
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, expogo())
