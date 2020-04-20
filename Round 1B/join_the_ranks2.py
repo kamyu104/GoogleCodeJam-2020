99164a9ad1bbc57cdb470b3038db9939eb68e729
Allen Liu <kamyu104@gmail.com>
2020-04-20 23:13:52 +0800

    update

--- Round 1B/join_the_ranks2.py
+++ Round 1B/join_the_ranks2.py
@@ -7,25 +7,21 @@
 # Space: O(R * S)
 #
 
-def find_len_A(deck):
-    for i in xrange(1, len(deck)):
-        if deck[i] != deck[0]:
-            break
-    for j in xrange(i+1, len(deck)):
-        if deck[j] != deck[i]:
-            break
-    return j
-
 def join_the_ranks():
     R, S = map(int, raw_input().strip().split())
-    result, deck = [], range(1, R+1)*S
+    result, groups = [], [1]*(R*S)
     for l in xrange(R+1, R*S, 2):  # each step, decrease the number of adjacent cards of different ranks from (R*S-1) to (R-1) by 2
-        len_A = find_len_A(deck)
+        len_A = groups[0]+groups[1]
         result.append((len_A, l-len_A))
-        deck[:] = deck[len_A:l] + deck[:len_A] + deck[l:]
+        for i in xrange(len(groups)):
+            l -= groups[i]
+            if not l:
+                break
+        groups[:] = groups[2:i] + [groups[i]+groups[0]] + [groups[1]+groups[i+1]] + groups[i+2:]
     if (R*S-R)%2:  # if odd, decrease the number of adjacent cards of different ranks from R to (R-1) by 1
-        result.append((S-1, len(deck)-(S-1)))  # in the last step, the ranks of the top S-1 cards and the last one must be all R, and the others are sorted
-        deck[:] = deck[S-1:] + deck[:S-1]
+        result.append((groups[0], R*S-groups[0]))  # in the last step, the ranks of the top S-1 cards and the last one must be all R, and the others are sorted
+        groups[:] = groups[1:-1] + [groups[-1]+groups[0]]
+    assert(len(groups) == R and all(x == groups[0] for x in groups))
     return "{}\n{}".format(len(result), "\n".join(map(lambda x: "{} {}".format(*x), result)))
 
 for case in xrange(input()):
