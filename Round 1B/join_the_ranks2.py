6482cbcc6dfa8f6bda6737e9a34f1cf1cca3e25e
kamyu <kamyu104@users.noreply.github.com>
2020-04-21 13:37:49 +0800

    Update join_the_ranks2.py

--- Round 1B/join_the_ranks2.py
+++ Round 1B/join_the_ranks2.py
@@ -3,7 +3,7 @@
 # Google Code Jam 2020 Round 1B - Problem C. Join the Ranks
 # https://codingcompetitions.withgoogle.com/codejam/round/000000000019fef2/00000000002d5b64
 #
-# Time:  O(R^2 * S)
+# Time:  O(R * S)
 # Space: O(R * S)
 #
 
@@ -12,11 +12,13 @@ def join_the_ranks():
     result, groups = [], [1]*(R*S)  # treat groups as stack
     for l in xrange(R+1, R*S, 2):  # each step, decrease the number of adjacent cards of different ranks from (R*S-1) to (R-1) by 2, and sort extra 2 cards without the last one
         result.append((groups[-1]+groups[-2], l-(groups[-1]+groups[-2])))
-        groups[-R-2:] = [groups[-R-2]+groups[-2]] + [groups[-1] + groups[-R-1]] + groups[-R:-2]  # Time: O(R)
+        groups[-R-2], groups[-R-1] = groups[-R-2]+groups[-2], groups[-1]+groups[-R-1]
+        groups.pop(), groups.pop()
     if (R*S-R)%2:  # if odd, decrease the number of adjacent cards of different ranks from R to (R-1) by 1
         assert(groups[-1] == S-1 and groups[0] == 1)
         result.append((groups[-1], R*S-groups[-1]))  # in the last step, the ranks of the top S-1 cards and the last one must be all R, and the others are sorted
-        groups[:] = [groups[-1]+groups[0]] + groups[1:-1]
+        groups[0] = groups[-1]+groups[0]
+        groups.pop()
     assert(len(groups) == R and all(x == groups[0] for x in groups))
     return "{}\n{}".format(len(result), "\n".join(map(lambda x: "{} {}".format(*x), result)))
 
