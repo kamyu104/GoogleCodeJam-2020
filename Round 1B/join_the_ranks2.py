9e03e01c0a4b2d5a3a614df812733f4517cf6685
Allen Liu <kamyu104@gmail.com>
2020-04-21 21:37:48 +0800

    update

--- Round 1B/join_the_ranks2.py
+++ Round 1B/join_the_ranks2.py
@@ -4,21 +4,24 @@
 # https://codingcompetitions.withgoogle.com/codejam/round/000000000019fef2/00000000002d5b64
 #
 # Time:  O(R * S)
-# Space: O(R * S)
+# Space: O(R)
 #
+
+from collections import deque
 
 def join_the_ranks():
     R, S = map(int, raw_input().strip().split())
-    result, groups = [], [1]*(R*S)  # treat groups as stack
+    result, groups = [], deque([1]*(R+2))
     for l in xrange(R+1, R*S, 2):  # each step, decrease the number of adjacent cards of different ranks from (R*S-1) to (R-1) by 2, and sort extra 2 cards without the last one
-        result.append((groups[-1]+groups[-2], l-(groups[-1]+groups[-2])))
-        groups[-R-2], groups[-R-1] = groups[-R-2]+groups[-2], groups[-1]+groups[-R-1]
-        groups.pop(), groups.pop()
-    if (R*S-R)%2:  # if odd, decrease the number of adjacent cards of different ranks from R to (R-1) by 1
-        assert(groups[-1] == S-1 and groups[0] == 1)
-        result.append((groups[-1], R*S-groups[-1]))  # in the last step, the ranks of the top S-1 cards and the last one must be all R, and the others are sorted
-        groups[0] = groups[-1]+groups[0]
-        groups.pop()
+        result.append((groups[0]+groups[1], l-(groups[0]+groups[1])))
+        groups[R], groups[R+1] = groups[R]+groups[0], groups[1]+groups[R+1]
+        groups.popleft(), groups.popleft()
+        groups.extend([1]*(min(R*S-(l+1), 2)))
+    if R*S-(l+1) == 1:  # if odd, decrease the number of adjacent cards of different ranks from R to (R-1) by 1
+        assert(groups[0] == S-1 and groups[-1] == 1)
+        result.append((groups[0], R*S-groups[0]))  # in the last step, the ranks of the top S-1 cards and the last one must be all R, and the others are sorted
+        groups[-1] = groups[-1]+groups[0]
+        groups.popleft()
     assert(len(groups) == R and all(x == groups[0] for x in groups))
     return "{}\n{}".format(len(result), "\n".join(map(lambda x: "{} {}".format(*x), result)))
 
