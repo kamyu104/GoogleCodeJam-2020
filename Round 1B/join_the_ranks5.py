230379dfced8ab55d0644822a7742951e2adb383
kamyu <kamyu104@users.noreply.github.com>
2020-04-22 13:13:37 +0800

    Update join_the_ranks5.py

--- Round 1B/join_the_ranks5.py
+++ Round 1B/join_the_ranks5.py
@@ -9,7 +9,7 @@
 
 def join_the_ranks():
     R, S = map(int, raw_input().strip().split())
-    return "{}\n{}".format(((S-1)*R+1)//2, "\n".join(map(lambda x: "{} {}".format(x[0], x[1]-x[0]), (((i//R+1)+((i+1)//R+1) if i+1 != (S-1)*R else (i//R+1), R+1+i) for i in xrange(0, (S-1)*R, 2)))))
+    return "{}\n{}".format(((S-1)*R+1)//2, "\n".join(map(lambda x: "{} {}".format(x[0]+x[1], x[2]-(x[0]+x[1])), (((i//R+1), ((i+1)//R+1) if i+1 != (S-1)*R else 0, R+1+i) for i in xrange(0, (S-1)*R, 2)))))
 
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, join_the_ranks())
