1929dfce039c35470a1528ab9eaa16c568e88fe4
kamyu <kamyu104@users.noreply.github.com>
2020-04-22 12:51:01 +0800

    Update join_the_ranks5.py

--- Round 1B/join_the_ranks5.py
+++ Round 1B/join_the_ranks5.py
@@ -3,14 +3,13 @@
 # Google Code Jam 2020 Round 1B - Problem C. Join the Ranks
 # https://codingcompetitions.withgoogle.com/codejam/round/000000000019fef2/00000000002d5b64
 #
-# Time:  O(R * S), optimized from join_the_ranks4.py
+# Time:  O(R * S), one-liner, optimized from join_the_ranks4.py
 # Space: O(1)
 #
 
 def join_the_ranks():
     R, S = map(int, raw_input().strip().split())
-    result = [((i//R+1)+((i+1)//R+1 if i+1 != (S-1)*R else 0), (R+1+i)-((i//R+1)+((i+1)//R+1 if i+1 != (S-1)*R else 0))) for i in xrange(0, (S-1)*R, 2)]
-    return "{}\n{}".format(len(result), "\n".join(map(lambda x: "{} {}".format(*x), result)))
+    return "{}\n{}".format(((S-1)*R+1)//2, "\n".join(map(lambda x: "{} {}".format(*x), [((i//R+1)+((i+1)//R+1 if i+1 != (S-1)*R else 0), R+1+i-((i//R+1)+((i+1)//R+1 if i+1 != (S-1)*R else 0))) for i in xrange(0, (S-1)*R, 2)])))
 
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, join_the_ranks())
