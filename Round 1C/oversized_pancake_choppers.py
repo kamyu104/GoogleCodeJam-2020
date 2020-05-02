f44b6ee6b3d1229d1df5171607a18f3e737797c3
Allen Liu <kamyu104@gmail.com>
2020-05-03 02:38:13 +0800

    update

--- Round 1C/oversized_pancake_choppers.py
+++ Round 1C/oversized_pancake_choppers.py
@@ -3,11 +3,11 @@
 # Google Code Jam 2020 Round 1C - Problem C. Oversized Pancake Choppers
 # https://codingcompetitions.withgoogle.com/codejam/round/000000000019fef4/00000000003179a1
 #
-# Time:  O(NlogN + Nlog(max(A)) + D * N * log(max(A)) + D * N)
+# Time:  O(NlogN + log(D * N) * N + D * N * log(max(A)) + D * N)
+#        = O(NlogN + D * N * log(max(A)))
 # Space: O(D * N)
 #
 
-from sys import float_info
 from collections import defaultdict
 from bisect import bisect_left
 
@@ -16,10 +16,11 @@ def gcd(a, b):  # Time: O(log(a + b))
         a, b = b, a % b
     return a
 
-def binary_search_right(left, right, check):  # Time: O(log(max(A)) * N)
+def binary_search_right(A, check):  # Time: O(log(D * N) * N)
+    left, right = 0, len(A)-1
     while left <= right:
         mid = left + (right-left)//2
-        if not check(mid):
+        if not check(A[mid]):
             right = mid-1
         else:
             left = mid+1
@@ -28,22 +29,17 @@ def binary_search_right(left, right, check):  # Time: O(log(max(A)) * N)
 def oversized_pancake_choppers():
     N, D = map(int, raw_input().strip().split())
     A = sorted(map(int, raw_input().strip().split()))  # Time: O(NlogN)
-    limit = (binary_search_right(1, max(A)*PRECISION, lambda a: sum(x*PRECISION//a for x in A) >= D),
-             PRECISION)
     lookup = defaultdict(lambda: [0])
     for y in xrange(1, D+1):  # Time: O(D * N * log(max(A)))
         for x in A:
-            if x*limit[1] > y*limit[0]:
-                break
             common = gcd(x, y)
             lookup[x//common, y//common].append(lookup[x//common, y//common][-1]+y)
+    targets = sorted(lookup.iterkeys(), cmp=lambda x, y: cmp(x[0]*y[1], x[1]*y[0]))
     M = 1
-    for k, count in lookup.iteritems():  # Time: O(D * N)
-        c = bisect_left(count, D)  # sum(len(count)) = O(D * N)
-        M = max(M, (c-int(count[c] != D)) if c != len(count) else c-1)
+    for i in xrange(binary_search_right(targets, lambda a: sum(x*a[1]//a[0] for x in A) >= D)+1):  # Time: O(D * N)
+        c = bisect_left(lookup[targets[i]], D)  # sum(len(count)) = O(D * N)
+        M = max(M, (c-int(lookup[targets[i]][c] != D)) if c != len(lookup[targets[i]]) else c-1)
     return D-M
 
-MAX_N = 10**4
-PRECISION = MAX_N**2
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, oversized_pancake_choppers())
