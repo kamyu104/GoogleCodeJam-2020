ead28b461fbe98f74a2f08ce71eb38afe08c51d2
Allen Liu <kamyu104@gmail.com>
2020-05-17 21:39:57 +0800

    update

--- Round 2/wormhole_in_one.py
+++ Round 2/wormhole_in_one.py
@@ -14,6 +14,16 @@ def gcd(a, b):  # Time: O(log(a + b))
         a, b = b, a % b
     return a
 
+def norm(a, b):
+    if b == 0:
+        a = 1
+    else:
+        if b < 0:
+            a, b = -a, -b
+        common = gcd(abs(a), b)
+        a, b = a//common, b//common
+    return a, b
+
 def wormhole_in_one():
     N = input()
     H = []
@@ -24,14 +34,7 @@ def wormhole_in_one():
         x2, y2 = H[j]
         for i in xrange(j):
             x1, y1 = H[i]
-            a, b = y2-y1, x2-x1
-            if b == 0:
-                a = 1
-            else:
-                if b < 0:
-                    a, b = -a, -b
-                common = gcd(abs(a), b)
-                a, b = a//common, b//common
+            a, b = norm(y2-y1, x2-x1)
             directions[a, b].add(i)
             directions[a, b].add(j)
     result = 0
