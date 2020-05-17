7ed1837d819804d234a9d913ebfb94f7ff050e0e
Allen Liu <kamyu104@gmail.com>
2020-05-17 16:38:10 +0800

    update

--- Round 2/security_update.py
+++ Round 2/security_update.py
@@ -9,24 +9,24 @@
 
 def security_update():
     C, D = map(int, raw_input().strip().split())
-    T_R = map(int, raw_input().strip().split())
-    T, R = [], []
-    for i, x in enumerate(T_R, 1):
-        if x > 0:
-            T.append((x, i))
-        elif x < 0:
+    R_T = map(int, raw_input().strip().split())
+    R, T = [], []
+    for i, x in enumerate(R_T, 1):
+        if x < 0:
             R.append((-x, i)) 
+        else:
+            T.append((x, i))
     assigned_T, last_merged = {0:0}, (0, 0, 0)
     R.sort(), T.sort()  # Time: O(ClogC)
     i, j = 0, 0
     while i != len(R) or j != len(T):
         if j == len(T) or (i != len(R) and R[i][0] <= len(assigned_T)):
-            last_merged = (last_merged[0] if R[i][0] == last_merged[1] else last_merged[0]+1, R[i][0], R[i][1])
-            assigned_T[R[i][1]] = last_merged[0]
+            last_merged = (R[i][0], last_merged[1] if R[i][0] == last_merged[0] else last_merged[1]+1)
+            assigned_T[R[i][1]] = last_merged[1]
             i += 1
         else:
-            last_merged = (T[j][0], last_merged[1] if T[j][0] == last_merged[0] else len(assigned_T), T[j][1])
-            assigned_T[T[j][1]] = last_merged[0]
+            last_merged = (last_merged[0] if T[j][0] == last_merged[1] else len(assigned_T), T[j][0])
+            assigned_T[T[j][1]] = last_merged[1]
             j += 1
     result = []
     for _ in xrange(D):  # Time: O(D)
