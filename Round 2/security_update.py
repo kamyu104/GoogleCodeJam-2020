42b44af97f60be41a21be7b71c2052d4d50873bc
Allen Liu <kamyu104@gmail.com>
2020-05-17 16:22:37 +0800

    update

--- Round 2/security_update.py
+++ Round 2/security_update.py
@@ -16,23 +16,22 @@ def security_update():
             T.append((x, i))
         elif x < 0:
             R.append((-x, i)) 
-    sorted_T_R = [(0, 0, 0)]
+    resolved_T, last_merged = {0:0}, (0, 0, 0)
     R.sort(), T.sort()
     i, j = 0, 0
     while i != len(R) or j != len(T):
-        if j == len(T) or (i != len(R) and R[i][0] <= len(sorted_T_R)):
-            sorted_T_R.append((sorted_T_R[-1][0] if R[i][0] == sorted_T_R[-1][1] else sorted_T_R[-1][0]+1, R[i][0], R[i][1]))
+        if j == len(T) or (i != len(R) and R[i][0] <= len(resolved_T)):
+            last_merged = (last_merged[0] if R[i][0] == last_merged[1] else last_merged[0]+1, R[i][0], R[i][1])
+            resolved_T[R[i][1]] = last_merged[0]
             i += 1
         else:
-            sorted_T_R.append((T[j][0], sorted_T_R[-1][1] if T[j][0] == sorted_T_R[-1][0] else len(sorted_T_R), T[j][1]))
+            last_merged = (T[j][0], last_merged[1] if T[j][0] == last_merged[0] else len(resolved_T), T[j][1])
+            resolved_T[T[j][1]] = last_merged[0]
             j += 1
-    final_T = [0]*C
-    for t, r, i in sorted_T_R:
-        final_T[i] = t
     result = []
     for _ in xrange(D):
         U, V = map(int, raw_input().strip().split())
-        result.append(max(abs(final_T[U-1]-final_T[V-1]), 1))
+        result.append(max(abs(resolved_T[U-1]-resolved_T[V-1]), 1))
     return " ".join(map(str, result))
     
 for case in xrange(input()):
