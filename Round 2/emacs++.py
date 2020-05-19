9c33f2f7e51e33afd357cc243d3f36f48adfab1a
kamyu <kamyu104@users.noreply.github.com>
2020-05-19 19:49:57 +0800

    Update emacs++.py

--- Round 2/emacs++.py
+++ Round 2/emacs++.py
@@ -149,7 +149,7 @@ def emacs():
     K, Q = map(int, raw_input().strip().split())
     PRG = raw_input().strip()
     L, R, P, S, E = [map(int, raw_input().strip().split()) for _ in xrange(5)]
-    result, pair, lookup, tree = 0, find_pair(PRG), {}, [[range(len(PRG)), -1, len(PRG)]]
+    pair, lookup, tree = find_pair(PRG), {}, [[range(len(PRG)), -1, len(PRG)]]
     return sum(query(PRG, L, R, P, pair, lookup, tree, 0, s-1, e-1) for s, e in izip(S, E))
 
 for case in xrange(input()):
