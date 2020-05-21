88e5d5815f7b03bf9d8453c3e04ee67d35fc259c
kamyu <kamyu104@users.noreply.github.com>
2020-05-21 18:47:55 +0800

    Update emacs++_slower.py

--- Round 2/emacs++_slower.py
+++ Round 2/emacs++_slower.py
@@ -16,20 +16,20 @@ from heapq import heappop, heappush
 from bisect import bisect_left
 
 def dijkstra(adj, t):  # Time: O(KlogK)
-    result, visited = {t:0}, set()
+    result, visited = {t:0}, set([t])
     min_heap = [(0, t)]
     while min_heap and len(visited) != len(adj):
         curr, u = heappop(min_heap)
-        if u in visited:
-            continue
-        visited.add(u)
+        new_q = {}
         for v, w in adj[u].iteritems():
-            if v in visited:
+            if (v in visited or
+                (v in result and result[v] <= curr+w) or
+                (v in new_q and new_q[v] <= curr+w)):
                 continue
-            if v in result and result[v] <= curr+w:
-                continue
-            result[v] = curr+w
-            heappush(min_heap, (curr+w, v))
+            new_q[v] = curr+w
+        for v, w in new_q.iteritems():
+            result[v] = w
+            heappush(min_heap, (w, v))
     return result
 
 def update_adj(lookup, brackets, adj, is_reversed, front, back, direction, dummy, d):  # Time: O(K)
