01c7f8118ff6c5370adf4d7ac22ab86ac76925bf
Allen Liu <kamyu104@gmail.com>
2020-06-10 23:59:44 +0800

    update

--- Round 3/naming_compromise.py
+++ Round 3/naming_compromise.py
@@ -31,10 +31,7 @@ def naming_compromise():
             j -= 1
         elif i and j and dp[i][j] == dp[i-1][j-1]+(S1[i-1] != S2[j-1]):
             mid -= (S1[i-1] != S2[j-1])
-            if S1[i-1] == S2[j-1]:  # same
-                result.append(S2[j-1])
-            else:  # replace
-                result.append(S2[j-1])
+            result.append(S2[j-1])
             i -= 1
             j -= 1
         else:
