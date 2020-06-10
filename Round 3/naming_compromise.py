846418ddae8c879322f256e20e3162ff12cd1f20
Allen Liu <kamyu104@gmail.com>
2020-06-10 23:39:33 +0800

    update

--- Round 3/naming_compromise.py
+++ Round 3/naming_compromise.py
@@ -19,35 +19,25 @@ def naming_compromise():
                 dp[i][j] = min(dp[i][j], dp[i][j-1]+1)
             if i and j:
                 dp[i][j] = min(dp[i][j], dp[i-1][j-1]+(S1[i-1] != S2[j-1]))
-    mid = dp[-1][-1]//2
-    i, j = len(S1), len(S2)
-    result = []
-    while i or j:
+    mid = dp[-1][-1]//2  # extra
+    result, i, j = [], len(S1), len(S2)
+    while (i or j) and mid:
         if i and dp[i][j] == dp[i-1][j]+1:  # delete
-            if mid:
-                mid -= 1
-            else:
-                result.append(S1[i-1])
+            mid -= 1
             i -= 1
         elif j and dp[i][j] == dp[i][j-1]+1:  # insert
-            if mid:
-                mid -= 1
-                result.append(S2[j-1])
+            mid -= 1
+            result.append(S2[j-1])
             j -= 1
         elif i and j and dp[i][j] == dp[i-1][j-1]+(S1[i-1] != S2[j-1]):
+            mid -= (S1[i-1] != S2[j-1])
             if S1[i-1] == S2[j-1]:  # same
                 result.append(S1[i-1])
             else:  # replace
-                if mid:
-                    mid -= 1
-                    result.append(S2[j-1])
-                else:
-                    result.append(S1[i-1])
+                result.append(S2[j-1])
             i -= 1
             j -= 1
-        else:
-            assert(False)
-    return "".join(reversed(result))
+    return S1[:i] + "".join(reversed(result))
 
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, naming_compromise())
