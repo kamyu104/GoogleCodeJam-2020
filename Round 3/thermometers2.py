f3adb090bcc7ac6c065baab7e032d40f706e3f27
Allen Liu <kamyu104@gmail.com>
2020-06-16 00:54:38 +0800

    update

---
+++ Round 3/thermometers2.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2020 kamyu. All rights reserved.
+#
+# Google Code Jam 2020 Round 3 - Problem B. Thermometers
+# https://codingcompetitions.withgoogle.com/codejam/round/000000000019ff7e/000000000037776b
+#
+# Time:  O(N^2)
+# Space: O(1)
+#
+
+def greedy(K, N, D, i):
+    result, left, right = 0, 0, D[i%len(D)]
+    for j in xrange(i+1, i+1+N):
+        left, right = min(D[(j-1)%len(D)]-right, D[j%len(D)]), min(D[(j-1)%len(D)]-left, D[j%len(D)])
+        if left >= right:
+            break
+        result += 1
+    else:
+        for j in reversed(xrange(i, i+N)):
+            left, right = D[j%len(D)]-right, D[j%len(D)]-left
+    return result, left, right
+
+def thermometers():
+    K, N = map(int, raw_input().strip().split())
+    X, T = [map(int, raw_input().strip().split()) for _ in xrange(2)]
+    D = [(X[(i+1)]-X[i])%K for i in xrange(len(X)-1)]
+    D.append(K-sum(D))
+    result, left, right = greedy(K, N, D, 0)
+    if result == N:
+        assert(left < right)
+        z = reduce(lambda x, y: y-x, D)
+        if not (N-1)%2:
+            if 2*left < z < 2*right:
+                return N
+        else:
+            if not z:
+                return N
+    result = N+(N//2)  # no adjacent segments with 2 thermometers
+    for i in xrange(N):
+        chain, j = 0, i
+        while j < i+N:
+            j += greedy(K, N, D, j%N)[0]
+            chain += 1
+        result = min(result, N+chain)
+    return result
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, thermometers())
