4e61c6f9d3269a523594ffc14c9ff925565935cb
Allen Liu <kamyu104@gmail.com>
2020-06-28 16:48:04 +0800

    update

--- Round 3/pen_testing_test.py
+++ Round 3/pen_testing_test.py
@@ -36,7 +36,7 @@ def careful_writing_prob(dead_mask, alive_used_count, lookup):  # Time: O(N)
 
 def memoization(dead_mask, alive_used_count, lookup):  # Time: O(N * states)
     if alive_used_count not in lookup[dead_mask]:
-        if USE_LEFTMOST:
+        if USE_LEFTMOST:  # there is no effect of normalization for careful only, so just skip if careful only
             min_count = min(next(i for i in xrange(N) if not (dead_mask & POW[i])), min(alive_used_count))
             if min_count:  # normalized to reduce the number of duplicated states from 1346148 to 832025
                 return memoization(reduce(or_, (POW[i-min_count] for i in xrange(N) if (dead_mask & POW[i]) or i-min_count < 0)), tuple(c-min_count for c in alive_used_count), lookup)
