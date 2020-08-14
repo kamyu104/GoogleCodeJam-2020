fe469d1bfb467351d93ed2da56b81ab41ee04e75
Allen Liu <kamyu104@gmail.com>
2020-08-15 00:43:54 +0800

    update

---
+++ Virtual World Finals/musical_cords.py
@@ -0,0 +1,110 @@
+# Copyright (c) 2020 kamyu. All rights reserved.
+#
+# Google Code Jam 2020 Virtual World Finals - Problem D. Musical Cords
+# https://codingcompetitions.withgoogle.com/codejam/round/000000000019ff31/00000000003b532b
+#
+# Time:  O(NlogN + N * K) on average
+# Space: O(N)
+#
+
+from math import sin, pi
+from functools import partial
+from collections import defaultdict
+from random import randint
+
+def nth_element(nums, n, compare=lambda a, b: a < b):
+    def partition_around_pivot(left, right, pivot_idx, nums, compare):
+        new_pivot_idx = left
+        nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
+        for i in xrange(left, right):
+            if compare(nums[i], nums[right]):
+                nums[i], nums[new_pivot_idx] = nums[new_pivot_idx], nums[i]
+                new_pivot_idx += 1
+
+        nums[right], nums[new_pivot_idx] = nums[new_pivot_idx], nums[right]
+        return new_pivot_idx
+
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        pivot_idx = randint(left, right)
+        new_pivot_idx = partition_around_pivot(left, right, pivot_idx, nums, compare)
+        if new_pivot_idx == n:
+            return
+        elif new_pivot_idx > n:
+            right = new_pivot_idx - 1
+        else:  # new_pivot_idx < n
+            left = new_pivot_idx + 1
+
+def hash(i, j):
+    if i > j:
+        return (j, i)
+    return (i, j)
+
+def f(R, D, L, i, j):
+    d = ((D[j]-D[i])%NANODEGREE_360)
+    d = min(d, NANODEGREE_360-d)
+    return 2*R*sin(d*pi/NANODEGREE_360)+L[j]
+
+def binary_search(left, right, check_fn): 
+    while left <= right:
+        mid = left + (right-left)//2
+        if check_fn(mid):
+            right = mid-1
+        else:
+            left = mid+1
+    return left
+
+def musical_cords():
+    def check_fn(N, R, D, L, prev, curr, x):
+        return f(R, D, L, x%N, curr%N) >= f(R, D, L, x%N, prev%N)
+
+    def check2_fn(N, D, i, x):
+        return 0 < (D[i%N]-D[x%N])%NANODEGREE_360 <= NANODEGREE_180
+
+    N, R, K = map(int, raw_input().strip().split())
+    D, L = [0]*N, [0]*N
+    for i in xrange(N):
+        D[i], L[i] = map(int, raw_input().strip().split())
+    
+    intervals = [[0, 0, 0]]
+    for i in xrange(1, 2*N):
+        left = i
+        while intervals and i-intervals[-1][0] < N and 0 < (D[i%N]-D[intervals[-1][0]%N])%NANODEGREE_360 <= NANODEGREE_180 and \
+              f(R, D, L, intervals[-1][0]%N, i%N) >= f(R, D, L, intervals[-1][0]%N, intervals[-1][2]%N) and \
+              f(R, D, L, intervals[-1][1]%N, i%N) >= f(R, D, L, intervals[-1][1]%N, intervals[-1][2]%N):
+            left = intervals[-1][0]
+            intervals.pop()  # remove fully covered and smaller
+        if intervals and 0 < (D[i%N]-D[intervals[-1][1]%N])%NANODEGREE_360 <= NANODEGREE_180:  # overlapped, shortened the previous interval
+            left = binary_search(intervals[-1][0], intervals[-1][1], partial(check2_fn, N, D, i))
+            assert(intervals[-1][0] <= left <= intervals[-1][1])
+            intersect = binary_search(left, intervals[-1][1], partial(check_fn, N, R, D, L, intervals[-1][2], i))
+            if left <= intersect <= intervals[-1][1]:
+                intervals[-1][1] = left = intersect
+            else:
+                left = intervals[-1][1]
+        intervals.append([left, i, i])
+    max_pair = [-1]*N
+    for left, right, j in intervals:
+        for i in xrange(left, right):
+            if max_pair[i%N] == -1 or f(R, D, L, i%N, j%N) > f(R, D, L, i%N, max_pair[i%N]):
+                max_pair[i%N] = j%N
+    pairs = defaultdict(int)
+    for i, j in enumerate(max_pair):
+        if j != -1:
+            pairs[hash(i, j)] = max(pairs[hash(i, j)], f(R, D, L, i, j)+L[i])
+    value_pairs = [(v, k) for k, v in pairs.iteritems()]
+    nth_element(value_pairs, K, compare=lambda a, b: a > b)
+    possible_pairs = defaultdict(int)
+    for _, pairs in value_pairs[:K]:
+        for i in pairs:
+            for j in xrange(N):
+                if j != i:
+                    possible_pairs[hash(i, j)] = max(possible_pairs[hash(i, j)], f(R, D, L, i, j)+L[i])
+    result = possible_pairs.values()
+    nth_element(result, K, compare=lambda a, b: a > b)
+    return " ".join(map(lambda x: "%.10f"%x, sorted(result[:K], reverse=True)))
+
+NANODEGREE_180 = 180*10**9
+NANODEGREE_360 = 360*10**9
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, musical_cords())
