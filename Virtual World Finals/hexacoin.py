57710b6e285b21dabf4f14f75fcf7bf0df040c65
Allen Liu <kamyu104@gmail.com>
2020-08-23 21:37:31 +0800

    update

--- Virtual World Finals/hexacoin.py
+++ Virtual World Finals/hexacoin.py
@@ -3,32 +3,39 @@
 # Google Code Jam 2020 Virtual World Finals - Problem C. Hexacoin
 # https://codingcompetitions.withgoogle.com/codejam/round/000000000019ff31/00000000003b4bc5
 #
-# Time:  O(B^(D + 1) * D + N^2 * D), PyPy2 TLE in test set 3
+# Time:  O(B^(D + 1) * D + N^2 * D), pass in PyPy2 but Python2
 # Space: O(B^(D + 1))
 #
 
-from fractions import gcd
 from collections import defaultdict
+from fractions import gcd
 
 def to_hex(x, D):  # Time: O(D)
-    return ("{:0" + str(D) +  "X}").format(x)
+    result = []
+    while len(result) != D:
+        result.append(x%B)
+        x //= B
+    result.reverse()
+    return result
 
 def find_structures(D, U):  # Time: O(B^(D + 1) * D)
     lookup = defaultdict(int)
     B_POW_D = B**D
     for x in xrange(min(U, B_POW_D)):  # O(B^D) times
         y = U-x
-        x_hex, y_hex = to_hex(x, D), to_hex(y, D)[:D]
+        x_hex, y_hex = to_hex(x, D), to_hex(y, D)
         norm, hash_value = {'*':0}, 0
-        for d, h in enumerate(x_hex+y_hex):  # O(B * D) times
-            h = int(h, B)
-            if d >= D:
-                if y >= B_POW_D:  # D "*" in the suffix
-                    lookup[hash_value] += 1
-                    break
-                for smaller_h in xrange(h):  # at most D-1 "*" in the suffix
-                    delta = norm[smaller_h] if smaller_h in norm else len(norm)
-                    lookup[hash_value*(B+1) + delta] += 1
+        for d, h in enumerate(x_hex):  # O(B * D) times
+            if h not in norm:
+                norm[h] = len(norm)
+            hash_value = hash_value*(B+1) + norm[h]
+        if y >= B_POW_D:  # all "*" in the y part
+            lookup[hash_value] += 1
+            continue
+        for d, h in enumerate(y_hex):  # O(B * D) times
+            for smaller_h in xrange(h):  # at most D-1 "*" in the suffix
+                delta = norm[smaller_h] if smaller_h in norm else len(norm)
+                lookup[hash_value*(B+1) + delta] += 1
             if h not in norm:
                 norm[h] = len(norm)
             hash_value = hash_value*(B+1) + norm[h]
@@ -39,12 +46,19 @@ def match_structures_and_count(N, L, lookup):  # Time: O(N^2 * D)
     for i in xrange(N):  # O(N) times
         for j in xrange(i+1, N):  # O(N) times
             norm, hash_value = {'*':0}, 0
-            for d, h in enumerate(L[i]+L[j]):  # O(D) times
+            for d, h in enumerate(L[i]):  # O(D) times
                 h = int(h, B)
                 if h not in norm:
                     norm[h] = len(norm)
                 hash_value = hash_value*(B+1) + norm[h]
-                if d >= len(L[i])-1 and hash_value in lookup:
+            if hash_value in lookup:  # all "*" in the y part
+                count += lookup[hash_value] * FACTORIAL[(B+1)-len(norm)]
+            for d, h in enumerate(L[j]):  # O(D) times
+                h = int(h, B)
+                if h not in norm:
+                    norm[h] = len(norm)
+                hash_value = hash_value*(B+1) + norm[h]
+                if hash_value in lookup:
                     count += lookup[hash_value] * FACTORIAL[(B+1)-len(norm)]
     return count
 
