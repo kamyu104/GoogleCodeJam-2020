f7bfd9b926d707977bdadfddf8964792c5b5c2e1
kamyu <kamyu104@users.noreply.github.com>
2020-08-25 19:17:55 +0800

    Update adjacent_and_consecutive2.py

--- Virtual World Finals/adjacent_and_consecutive2.py
+++ Virtual World Finals/adjacent_and_consecutive2.py
@@ -130,9 +130,9 @@ def is_B_winning_state(tiles, cells, Lt, Lt_lookup, Lt_Z, Lc, Lc_lookup, Lc_Z, K
     tiles[i], cells[j] = j, i
     Lt_Z_delta = update_L(Lt, Lt_lookup[i], 1)
     Lc_Z_delta = update_L(Lc, Lc_lookup[j], 1)
-    can_B_win =  not ((count_of_3_or_up(Lt) and count_of_3_or_up(Lc)) or
-                      (K == 2 and (Lt_Z+Lt_Z_delta) == (Lc_Z+Lc_Z_delta) == 1) or
-                      (K%2 and 2*((Lt_Z+Lt_Z_delta) + (Lc_Z+Lc_Z_delta)) > K))
+    can_B_win = not ((count_of_3_or_up(Lt) and count_of_3_or_up(Lc)) or
+                     (K == 2 and (Lt_Z+Lt_Z_delta) == (Lc_Z+Lc_Z_delta) == 1) or
+                     (K%2 and 2*((Lt_Z+Lt_Z_delta) + (Lc_Z+Lc_Z_delta)) > K))
     update_L(Lc, Lc_lookup[j], -1)
     update_L(Lt, Lt_lookup[i], -1)
     tiles[i], cells[j] = -2, -2
